a3ba551493dd60cb29559c6a73b167fa542a5a97
tian <lctianpeng@163.com>
2018-02-27 15:11:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tp/zhihunews_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tp.zhihunews_kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
